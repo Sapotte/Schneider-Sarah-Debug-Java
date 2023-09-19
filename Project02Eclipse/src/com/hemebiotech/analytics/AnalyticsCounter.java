df8b8e6eb7ceaec853b53010b8f5ec044e47e24b
Sarah Schneider <sarahschneider262@gmail.com>
2023-09-19 11:19:29 +0200

    change comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,15 +4,17 @@ import java.util.Map;
 
 public class AnalyticsCounter {
 	
-	public static void main(String[] args) {
-		// first get input
+	public static void main(String[] args) throws Exception {
 
 		try {
+
 			ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile();
 			WriteSymptomDataFromFile writeSymptomDataFromFile = new WriteSymptomDataFromFile();
 
+			//get input
 			Map<String, Integer> symptoms = readSymptomDataFromFile.GetSymptoms("C:\\Users\\sarah\\DepotGit\\OpenClassrooms\\Schneider-Sarah-Debug-Java\\Project02Eclipse\\src\\com\\hemebiotech\\analytics\\symptoms.txt");
 
+//			write the output
 			writeSymptomDataFromFile.writeSymptoms(symptoms);
 		} catch (Exception e) {
 			e.printStackTrace();
