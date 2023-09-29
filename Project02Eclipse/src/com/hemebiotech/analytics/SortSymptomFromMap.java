7a3d5937d86250905e2da8ee4632368dd80b4806
Sarah Schneider <sarahschneider262@gmail.com>
2023-09-29 21:07:22 +0200

    change interfaces

--- Project02Eclipse/src/com/hemebiotech/analytics/SortSymptomFromMap.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SortSymptomFromMap.java
@@ -8,8 +8,8 @@ public class SortSymptomFromMap implements ISymptomSorter {
     /**
      * Sorts the entries of a Map
      *
-     * @param countedSymptoms
-     * @return sortedSymptoms
+     * @param countedSymptoms symptoms and their occurrences
+     * @return sortedSymptoms symptoms sort in alphabetical order
      */
     public TreeMap<String, Integer> sortSymptoms(Map<String, Integer> countedSymptoms) {
         Map<String, Integer> sortedSymptoms = new TreeMap<>();
