d30c830acd6a72a9485835adcc05fb1f12b304f5
Sarah Schneider <sarahschneider262@gmail.com>
2023-09-29 14:09:53 +0200

    Add interface and class to sort the symptoms (fix)

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,6 +1,7 @@
 package com.hemebiotech.analytics;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 
@@ -8,13 +9,15 @@ public class AnalyticsCounter {
 
         try {
             ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile();
+            SortSymptomFromMap sortSymptomFromMap = new SortSymptomFromMap();
             WriteSymptomDataFromFile writeSymptomDataFromFile = new WriteSymptomDataFromFile();
 
 
             Map<String, Integer> symptoms = readSymptomDataFromFile.GetSymptoms("Project02Eclipse/src/symptoms.txt");
+            TreeMap<String, Integer> sortedSymptoms = sortSymptomFromMap.sortSymptoms(symptoms);
 
 
-            writeSymptomDataFromFile.writeSymptoms(symptoms);
+            writeSymptomDataFromFile.writeSymptoms(sortedSymptoms);
         } catch (Exception e) {
             e.printStackTrace();
         }
